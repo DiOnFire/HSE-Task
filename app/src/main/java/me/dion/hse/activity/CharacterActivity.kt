bb6ee9fda2f12c1f130d13a046806129272b0608
DiOnFire <di0nik.gd@gmail.com>
2022-07-07 16:22:13 +0300

    Character activity!

--- app/src/main/java/me/dion/hse/activity/CharacterActivity.kt
+++ app/src/main/java/me/dion/hse/activity/CharacterActivity.kt
@@ -1,13 +1,46 @@
 package me.dion.hse.activity
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import me.dion.hse.R
+import me.dion.hse.network.NetBitmap
 import me.dion.hse.traits.Character
 
 class CharacterActivity : AppCompatActivity() {
+    private lateinit var character: Character
+    private lateinit var characterImg: ImageView
+    private lateinit var characterName: TextView
+    private lateinit var characterId: TextView
+    private lateinit var characterSpecies: TextView
+    private lateinit var characterStatus: TextView
+    private lateinit var characterGender: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_character)
+
+        character = intent.getSerializableExtra("character") as Character
+        characterImg = findViewById(R.id.characterImg)
+        characterName = findViewById(R.id.charName)
+        characterId = findViewById(R.id.charId)
+        characterSpecies = findViewById(R.id.charSpecies)
+        characterStatus = findViewById(R.id.charStatus)
+        characterGender = findViewById(R.id.charGender)
+
+        applyMetadata()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun applyMetadata() {
+        val thread = NetBitmap(character.image, characterImg)
+        thread.start()
+        characterName.text = "Name: ${character.name}"
+        characterId.text = "UUID: ${character.id}"
+        characterSpecies.text = "Species: ${character.species}"
+        characterStatus.text = "Status: ${character.status}"
+        characterGender.text = "Gender: ${character.gender}"
     }
 }
--- app/src/main/java/me/dion/hse/activity/SearchActivity.kt
+++ app/src/main/java/me/dion/hse/activity/SearchActivity.kt
@@ -1,4 +1,46 @@
 package me.dion.hse.activity
 
-class SearchActivity {
+import android.content.ClipData.newIntent
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import me.dion.hse.R
+import me.dion.hse.traits.Character
+
+class SearchActivity : AppCompatActivity() {
+    private lateinit var characters: MutableList<Character>
+    private lateinit var searchList: ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+        searchList = findViewById(R.id.searchList)
+        characters = intent.getSerializableExtra("characters") as MutableList<Character>
+
+        searchList.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, convertToString(characters))
+
+        searchList.setOnItemClickListener { _, item, _, _ ->
+            val textView = item as TextView
+            val charName = textView.text.toString()
+            val character = characters.find { it.name == charName }
+            val intent = Intent(
+                this,
+                CharacterActivity::class.java
+            )
+            intent.putExtra("character", character)
+            startActivity(intent)
+        }
+    }
+
+    private fun convertToString(characters: List<Character>): MutableList<String> {
+        val strings = mutableListOf<String>()
+        for (character in characters) {
+            strings.add(character.name)
+        }
+        return strings
+    }
 }
